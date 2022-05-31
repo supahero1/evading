d1f433353150d4fef16b214dff5eed16b8fbdd49
supahero1 <user@example.com>
2022-05-31 15:23:09 +0200

    main

--- install_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /evading/server
-apt install npm nodejs net-tools
-ufw enable
-ufw allow 443
-npm i yarn -g
-yarn add uWebSockets.js@uNetworking/uWebSockets.js\#v20.10.0
-iptables -A INPUT -p tcp --syn --dport 443 -m connlimit --connlimit-above 1 --connlimit-mask 32 -j REJECT --reject-with tcp-reset
--- install_website.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /evading/website
-apt install npm nodejs net-tools
-ufw enable
-ufw allow 443
-npm i express
-npm i -g google-closure-compiler
