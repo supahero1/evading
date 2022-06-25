87c2451c27321fd3cdac6605362f472656f37bb5
supahero1 <user@example.com>
2022-06-25 17:54:59 +0200

    main

--- website.sh
+++ website.sh
@@ -4,6 +4,7 @@ rm -fr /evading
 cd /
 git clone git@github.com:supahero1/evading
 cd evading
+npm install cssnano postcss postcss-cli --save-dev
 make sed WEBSITE_NAME=evading.shadam.xyz SERVER_NAME=frankfurt.shadam.xyz SECURE_WEBSITE=1 SECURE_SERVER=1
 cd website
 npm i express
