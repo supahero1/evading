bc9cec65b02adda70c06f8bc5de03690fe422830
supahero1 <user@example.com>
2022-07-19 18:47:03 +0200

    last fix i swear

--- server/main.c
+++ server/main.c
@@ -1755,7 +1755,14 @@ static void parse(void) {
             }
             uint8_t i = client->spectating_client_id;
             while(1) {
-              i = (uint8_t)(i + op) % max_players;
+              if(op == 1) {
+                i = (i + 1) % max_players;
+              } else {
+                i = i + 255;
+                if(i == 255) {
+                  i = max_players - 1;
+                }
+              }
               if(!clients[i].exists) continue;
               client_set_spectated_player(client_id, i);
               break;
