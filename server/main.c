5efe8f7c33bbe0c491b431dfed256d6ec5bf2286
supahero1 <user@example.com>
2022-07-28 17:34:01 +0200

    fix 2

--- server/main.c
+++ server/main.c
@@ -1251,9 +1251,11 @@ static void player_collide_ball(const uint8_t client_id, struct grid_entity* con
     client->updated_dc = 1;
     if(ball->speed <= 0.001) {
       const float angle = atan2f(client->entity.y - ball_entity->y, client->entity.x - ball_entity->x);
-      const float r_total = client->entity.r + ball_entity->r + 1;
+      const float r_total = client->entity.r + ball_entity->r + 0.01f;
       client->entity.x = ball_entity->x + cosf(angle) * r_total;
       client->entity.y = ball_entity->y + sinf(angle) * r_total;
+      client->updated_x = 1;
+      client->updated_y = 1;
       grid_recalculate(&areas[client->area_id].grid, &client->entity);
     }
   }
